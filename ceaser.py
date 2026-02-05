ac6a416ec691d4a51b48ce21f181b22f7672a659
Pokol <petrenyio@gmail.com>
2026-02-05 19:49:21 +0100

    kesz is van

--- ceaser.py
+++ ceaser.py
@@ -1,8 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
@@ -64,4 +62,18 @@ def ceasar(direction):
 #decrypt(text, shift, alphabet)
 #encrypt(text, shift, alphabet)
 
-ceasar(direction)
+more = True
+
+while(more):
+    print("Welcome to the coder/decoder machine!")
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    ceasar(direction)
+
+    choice = input("Do you wanna continue?(y/n): ")
+
+    if choice == "n":
+        more = False
+
